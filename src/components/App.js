f33e7b6f9a92d72bedacef9cbbc4b850eda998ee
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-01-15 16:58:44 +0530

    tse rewuired changes made

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 
-function App() {
-  return (
-    <div className="App">
-      <BooksList />
-      <BooksForm />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <BooksList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
